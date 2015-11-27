7c6d0c83a468b99087423b272a75a91f8fbde042
pcali41 <pcali41@gwmail.gwu.edu>
2015-11-27 00:21:53 -0500

    Added port connection functions
    I added a client-server port connection between the popup and content
    script. The popup should now be able to send messages to the content
    script and vise-versa if necessary.

--- src/c_frame/changer.js
+++ src/c_frame/changer.js
@@ -1,48 +1,61 @@
 
-/**
-*This will call other functions 
-*such as retrieve elements. 
-*/
-function setColors()
-{
-	//This is a test to show how to compare the btype and change an element
-	if(btype == "red")
-	{
-		chrome.tabs.executeScript(null,
-      {code:"document.body.style.backgroundColor='red'"});//we change the color of background element
-	}
-	else if(btype == "blue")
-	{
-		chrome.tabs.executeScript(null,
-      {code:"document.body.style.backgroundColor='blue'"});//we change the color of background element
-	}
-}
-/**
-*This will analyze the colors values of a specific element and change those
-*values to something that is easier to read. It will determine if it needs
-*/
-function changeValues()
-{
-	/**
-	*This will be the variable(s) used in calculating the new color
-	*values. Depending on what btype is we will set the contrast
-	*to a certain value.
-	*/
-	var contrast;
-	if(btype == "red")
-	{
-		contrast = 1;
-	}
-	else if(btype == "blue")
-	{
-		contrast = 2;
-	}
-}
-/**
-*This will get the elements of the page, and then call
-*changeValues() on each element. Will require traversing the dom tree
-*/
-function retrieveElements()
-{
+//Initialize port connection with popup script
+var port = chrome.runtime.connect({name: "color_port"});
 
-}
+//Receive and service color change requests
+port.onMessage.addListener(function(msg) {
+	
+	//Print message to the console
+	console.log("message received: \"" + msg.text + "\"");
+	//Get blind type from port message
+	var btype = msg.text; 
+	
+	/**
+	*This will call other functions 
+	*such as retrieve elements. 
+	*/
+	function setColors()
+	{
+		//This is a test to show how to compare the btype and change an element
+		if(btype == "red")
+		{
+			chrome.tabs.executeScript(null,
+				{code:"document.body.style.backgroundColor='red'"});//we change the color of background element
+		}
+		else if(btype == "blue")
+		{
+			chrome.tabs.executeScript(null,
+				{code:"document.body.style.backgroundColor='blue'"});//we change the color of background element
+		}
+	}
+	/**
+	*This will analyze the colors values of a specific element and change those
+	*values to something that is easier to read. It will determine if it needs
+	*/
+	function changeValues()
+	{
+		/**
+		*This will be the variable(s) used in calculating the new color
+		*values. Depending on what btype is we will set the contrast
+		*to a certain value.
+		*/
+		var contrast;
+		if(btype == "red")
+		{
+			contrast = 1;
+		}
+		else if(btype == "blue")
+		{
+			contrast = 2;
+		}
+	}
+	/**
+	*This will get the elements of the page, and then call
+	*changeValues() on each element. Will require traversing the dom tree
+	*/
+	function retrieveElements()
+	{
+
+	}
+	
+});
--- src/c_frame/popup.js
+++ src/c_frame/popup.js
@@ -1,9 +1,15 @@
 
 var btype;
 
-function click(e) {
-	btype = e.target.id;//get the id of the button (btype is short for 'blind type')
-  	setColors();
-	//close the window
-	window.close();
-}
+//Accept port connection with content script
+chrome.runtime.onConnect.addListener(function(port) {
+	if (port.name != "color_port") return;//Confirm port name
+
+	function click(e) {
+		btype = e.target.id;//get the id of the button (btype is short for 'blind type')
+		port.postMessage({text: btype});
+		window.close();//close the window
+
+	}
+
+});
