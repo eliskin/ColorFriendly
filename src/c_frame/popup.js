e9a8730b1dd972b03a2ff36e53ace740adbb1eae
theawesomeguy111 <elliotliskin@gwmail.gwu.edu>
2015-12-05 01:35:43 -0500

    Created a Checkbox that Persists Fade Settings
    -Made permissions easier to read by listing them on multiple lines (instead of just one)
    -Added “storage” to the permissions
    -Changed the title of the popup to a more general name
    -Chose more accurate class names for the popup divs
    -Created a checkbox that will toggle on and off whether the fade animation occurs
    -Implemented storage to persist the user’s fade animation selection

--- src/c_frame/popup.js
+++ src/c_frame/popup.js
@@ -1,10 +1,33 @@
+var kBTypeKey = "kBTypeKey";
+var kFadeOnOff = "kFadeOnOff";
+
 //Get bg_page object
 var bgpage = chrome.extension.getBackgroundPage();
-	
+
+function checkboxDidChange(e) {
+    // the value of checkbox.checked is the value of the new state
+    // of the checkbox (so if the user is unchecking the box,
+    // checkbox.checked returns false)
+    var checked = e.target.checked;
+    chrome.storage.sync.set({kFadeOnOff: checked}, function(result) {
+                            if (chrome.runtime.lastError)
+                                console.warn(chrome.runtime.lastError.message);
+                            });
+}
+
 //Add listener for color selections
 document.addEventListener('DOMContentLoaded', function () {
-	var span = document.getElementById("s")
-	span.addEventListener('click', click);
+                          var span = document.getElementById("s");
+                          span.addEventListener('click', click);
+
+                          var checkbox = document.getElementById("FadeToggler");
+                          chrome.storage.sync.get(kFadeOnOff, function(result) {
+                                                  if (chrome.runtime.lastError)
+                                                    console.warn(chrome.runtime.lastError.message);
+                                                  else if (typeof result.kFadeOnOff !== "undefined")
+                                                    checkbox.checked = result.kFadeOnOff;
+                                                  });
+                          checkbox.addEventListener('change', checkboxDidChange);
 });
 
 //Forward color choice to bg_page.js
