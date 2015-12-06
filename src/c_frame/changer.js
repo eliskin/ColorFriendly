1166eec06dfa139ebd4c69872688a2fc58f17c6c
Ian Davila <nanotane1@gmail.com>
2015-12-06 17:25:23 -0500

    added changeValue
    completed the changeValue function which will alter a given value and
    makes sure that value stays within a certain range

--- src/c_frame/changer.js
+++ src/c_frame/changer.js
@@ -5,6 +5,7 @@ var red  = "red";
 //RED AND BLUE HUE/COLOR MODIFIERS
 var contrastR = 1;// red green colorblind value
 var contrastB = 1;//blue green colorblind value
+var contrast;
 var btype = "UNDEF";
 //Initialize port connection with popup script
 var port = chrome.runtime.connect({name: "color_port"});
@@ -32,39 +33,62 @@ port.onMessage.addListener(function(msg) {
 	function setColors()
 	{
 		//This is a test to show how to compare the btype and change an element
-		if(btype == red)
+		var 2darray = msg.text;
+		if (btype == red) 
 		{
 			console.log("Setting for red...")
+			contrast = contrastR;
 		}
-		else if(btype == blue)
+		else if (btype == blue)
 		{
 			console.log("Setting for blue...")
+			contrast = contrastB;
+		}
+			
+		/*
+		*How the color objects are set up:
+		*object{
+		*	name 
+		*	r
+		*	g
+		*	b
+		*	c
+		*	}
+		*The array is 2D, each section
+		*represents the background and forground color of an html object
+		*So for example, [x][0] is the forground color of some objext x
+		*and [x][1] is the background color of some object x
+		*/
+
+		//This will traverse every html object
+		for(int i = 0; i < 2darray; i++)
+		{
+			//This will traverse the background and the forground color of an object
+			var oldc = 2darray[i][0].c;
+
 		}
 	}
 	/**
-	*This will analyze the colors values of a specific element and change those
-	*values to something that is easier to read. It will determine if it needs
+	*This will change the value given by adding contrast to it. If the
+	*new value is too low or too high we will wrap the number around
 	*/
-	function changeValues()
+	function changeValue(value)
 	{
-		/**
-		*This will be the variable(s) used in calculating the new color
-		*values. Depending on what btype is we will set the contrast
-		*to a certain value.
-		*/
+		var lowest = 0; //the lowest we can go
+		var highest = 255;//the highest we can go
 
-		//set contrast
-		var contrast;
-		if(btype == red)
+		//may change this to multiplication or something
+		var newValue = value + contrast;
+
+		if(newValue > highest)//if too high
 		{
-			contrast = 1;
+			newValue = newValue - highest;
 		}
-		else if(btype == blue)
+		else if(newValue < lowest) // if too low
 		{
-			contrast = 2;
+			newValue = newValue + highest;
 		}
-
-
+		return newValue;
 	}
 	
 });
