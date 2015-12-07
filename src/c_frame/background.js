3d0559e04b9c23dbf29fc2df4659d0a24b0d3544
pcali41 <pcali41@gwmail.gwu.edu>
2015-12-06 22:55:37 -0500

    added auto-update feature for page reloading

--- src/c_frame/background.js
+++ src/c_frame/background.js
@@ -3,8 +3,18 @@ var color_port; //'blind type'
 
 //Accept port connection with content script
 chrome.runtime.onConnect.addListener(function(port) {
-	  if (port.name != "color_port") return;//Confirm port name
-	  color_port = port;
+    if (port.name != "color_port") return;//Confirm port name
+    color_port = port;
+
+    var kBTypeKey = "kBTypeKey";
+    var kFadeOnOff = "kFadeOnOff";
+
+    chrome.storage.sync.get(kBTypeKey, function(result) {
+        send(result.kBTypeKey);
+    });
+    chrome.storage.sync.get(kFadeOnOff, function(result) {
+        send(result.kFadeOnOff);
+    });
 });
 
 //Forward color choice to changer.js
