32eee5613fac9a0221eb59ba929c17c12a728392
theawesomeguy111 <elliotliskin@gwmail.gwu.edu>
2015-12-07 13:15:03 -0500

    Fixed Disabling Adjustments Checkbox Not Working
    -Also switched an if to an else if to prevent a situation in which there is an error but program flow continues when loading the kBTypeKey from storage

--- src/c_frame/popup.js
+++ src/c_frame/popup.js
@@ -23,8 +23,8 @@ document.addEventListener('DOMContentLoaded', function () {
             console.warn(chrome.runtime.lastError.message);
         // the first time this runs, undefined is returned,
         // and we don't need another if for that case because
-        // the default value of the checkbox will just be true		
-  	    if (typeof result.kBTypeKey == "string" && result.kBTypeKey != "default") {
+        // the default value of the checkbox will just be true
+        else if (typeof result.kBTypeKey == "string" && result.kBTypeKey != "default") {
   	        choice = document.getElementById(result.kBTypeKey);
   	        choice.style.background = kSelectedButtonBGColor;
   	    }
@@ -70,8 +70,17 @@ function adjustmentCheckboxDidChange(e) {
     // of the checkbox (so if the user is unchecking the box,
     // checkbox.checked returns false)
     var checked = e.target.checked;
-    // [TODO] Should there be a different function for this send and for
-    // the send of the other checkbox?
+    if (!checked) {
+        choice = "default";
+        bgpage.send(choice);
+    } else {
+        chrome.storage.sync.get(kBTypeKey, function(result) {
+            if (chrome.runtime.lastError)
+                console.warn(chrome.runtime.lastError.message);
+            else if (typeof result.kBTypeKey == "string" && result.kBTypeKey != "default")
+                choice = document.getElementById(result.kBTypeKey);
+        });
+    }
     bgpage.send(checked);
     chrome.storage.sync.set({kAdjustmentOnOff: checked}, function(result) {
         if (chrome.runtime.lastError)
